c725d0ff4f7a002065eadba5626659fbd7b1e621
OH Seongho <48385389+tjd985@users.noreply.github.com>
2023-04-11 15:14:02 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 34.95 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,8 @@
+function solution(arr){
+  let answer = [];
+  answer.push(arr[0]);
+  for(let i=0; i<arr.length-1;i++){
+    if(arr[i] !== arr[i+1]) answer.push(arr[i+1]);
+  }
+  return answer;
+}
