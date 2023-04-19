962afe0236842c9bf4bccfffe54e3d7fca318db2
OH SeongHo <tjd985@gmail.com>
2023-04-19 18:23:40 +0900

    [level 1] Title: 실패율, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,21 +1,12 @@
 function solution(n, stages) {
-  let trying = [];
-  let fail = [];
+  let answer = [];
   
   for(let i=1; i<=n; i++){
-    let cnt = 0;
-    let failcnt = 0;
-    
-    for(let j=0; j<stages.length; j++){
-      if(stages[j] >= i){
-        cnt++;
-        if(stages[j] === i) failcnt++;
-      }
-    }
-    trying.push(cnt);
-    fail.push(failcnt);
+    let trying = stages.filter((index) => index >= i).length;
+    let fail = stages.filter((index) => index === i).length;
+    answer.push([i, fail/trying]);
   }
   
-  let failure = trying.map((index,i) => [i+1,fail[i] / index]).sort((a,b) => b[1]-a[1]);
-  return failure.map((index) => index[0]);
+  answer.sort((a,b) => b[1]-a[1]);
+  return answer.map((index) => index[0]);
 }
