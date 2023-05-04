584891004719833f2d7fc5a485a9a941d50b4917
OH SeongHo <tjd985@gmail.com>
2023-05-04 17:36:08 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,13 +1,10 @@
 function solution(brown, yellow) {
   let sub = [];
-  let a = 0;
   for(let i=1; i<=Math.floor(brown / 2); i++){
     if(yellow%i === 0){
       let answer = 2*(i + yellow/i + 2);
       if(answer === brown){
-        let dap = [i, yellow/i].sort((a,b) => b-a);
-        dap[0] += 2;
-        dap[1] += 2;
+        let dap = [i, yellow/i].sort((a,b) => b-a).map((index) => index+2);
         return dap;
         break;
       }
