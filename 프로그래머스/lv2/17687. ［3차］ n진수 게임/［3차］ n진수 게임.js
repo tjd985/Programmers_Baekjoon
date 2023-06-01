5c462837aadc915c371387eaa3298fa6490c4b41
OH SeongHo <tjd985@gmail.com>
2023-06-01 15:53:42 +0900

    [level 2] Title: [3차] n진수 게임, Time: 1.10 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
@@ -0,0 +1,22 @@
+function solution(n, t, m, p) {
+  let str = "0";
+  let cnt = 0;
+  p = p-1;
+  let answer = [];
+  const a = ["A", "B", "C", "D", "E", "F"];
+  
+  while(str.length < t*m){
+    cnt++;
+    if(n > 10 && cnt >= 10 && cnt < n){
+      str += a[cnt%10];
+    }
+    else str += cnt.toString(n).toUpperCase();
+  }
+  
+  for(let i=p; i<str.length; i = i+m){
+    if(answer.length === t) break;
+    answer.push(str[i]);
+  }
+  
+  return answer.join("");
+}
