7e808ce68aca02eaddedc7b71bade0e47e1fb0ec
OH SeongHo <tjd985@gmail.com>
2023-09-23 13:11:03 +0900

    [level 1] Title: K번째수, Time: 0.06 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,25 +1,9 @@
 function solution(array, com){
-  
-  function Sort(arr){
-    let temp = 0;
-    for(let i=arr.length-1; i>0; i--){
-      for(let j = 0; j<i; j++){
-        if(arr[j] > arr[j+1]){
-          temp = arr[j];
-          arr[j] = arr[j+1];
-          arr[j+1] = temp;
-        }
-      }
-    }
-    return arr;
-  }
-  
-  let answer = [];
-  
-  for(let i =0; i<com.length;i++){
-    arr = array.slice(com[i][0]-1, com[i][1]);
-    Sort(arr);
-    answer.push(arr[com[i][2]-1]);
-  }
-  return answer;
+    const result = [];
+    com.forEach((arr) => {
+      const miniArr = array.slice(arr[0]-1, arr[1]);
+        miniArr.sort((a,b) => a-b);
+      result.push(miniArr[arr[2]-1]);
+    });
+  return result;
 }
