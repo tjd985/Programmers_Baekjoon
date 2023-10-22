5a025cf97f2a653f54d6405ec510498046384b83
OH SeongHo <tjd985@gmail.com>
2023-10-22 17:20:41 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 124 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.js
@@ -0,0 +1,5 @@
+let fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split(" ");
+
+console.log(Number(input[0]) - 543);
