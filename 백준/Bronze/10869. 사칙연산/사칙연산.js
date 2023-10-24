e4668f0c2484cbb7b82efa54e72020e75b1a9724
OH SeongHo <tjd985@gmail.com>
2023-10-24 13:09:49 +0900

    [Bronze V] Title: 사칙연산, Time: 124 ms, Memory: 9612 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,13 @@
+let fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+// let input = fs.readFileSync(filePath).toString().trim().split("\n");
+let input = fs.readFileSync(filePath).toString().trim().split(" ");
+input = input.map((value) => Number(value));
+
+const [a, b] = [...input];
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(Math.floor(a / b));
+console.log(a % b);
