f0e649c6ffab7d4abfacd683116ce5c4a780d834
OH SeongHo <tjd985@gmail.com>
2023-12-16 15:30:37 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -2,13 +2,11 @@ function solution(n, arr1, arr2) {
   return Array(n).fill("").map((value, index) => {
      const binary1 = arr1[index].toString(2);
      const binary2 = arr2[index].toString(2);
-     const zeroString1 = "0".repeat(n - binary1.length);
-     const zeroString2 = "0".repeat(n - binary2.length);
-     const formatedRow1List = (zeroString1 + binary1).split("").map(Number);
-     const formatedRow2List = (zeroString2 + binary2).split("").map(Number);
+     const formatedRow1List = binary1.padStart(n, 0);
+     const formatedRow2List = binary2.padStart(n, 0);
      
      for (let i = 0; i < n; i++) {
-       const isWall = !!(formatedRow1List[i] || formatedRow2List[i]);
+       const isWall = !!(Number(formatedRow1List[i]) || Number(formatedRow2List[i]));
        
        value += isWall ? "#" : " ";
      }
