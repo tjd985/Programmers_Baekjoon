b530973d0e692d63494b9b7cfde6f030d06b231e
OH SeongHo <tjd985@gmail.com>
2024-07-16 19:43:18 +0900

    [Silver II] Title: A → B, Time: 96 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.js
+++ 백준/Silver/16953. A → B/A → B.js
@@ -2,27 +2,35 @@ const INPUT_PATH = "../inputs/AB.txt";
 
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : INPUT_PATH;
-const input = fs.readFileSync(filePath).toString().trim().split(" ");
-const [startNum, targetNum] = input.map(Number);
-let result = Infinity;
+const input = fs.readFileSync(filePath).toString().trim();
+// .map(Number);
+// const [startNum, targetNum] = input.map(Number);
 
-function solution(currentNum, count) {
-  if (Number(currentNum) > targetNum) {
-    return;
+function solution(inputArguments) {
+  const [A, B] = inputArguments.split(" ").map(Number);
+  const queue = [[B, 1]];
+
+  while (queue.length > 0) {
+    const [current, count] = queue.shift();
+
+    if (current === A) {
+      return count;
+    }
+
+    if (current < A) {
+      break;
+    }
+
+    if (current % 2 === 0) {
+      queue.push([current / 2, count + 1]);
+    }
+
+    if (current % 10 === 1) {
+      queue.push([Math.floor(current / 10), count + 1]);
+    }
   }
 
-  if (Number(currentNum) === targetNum) {
-    result = Math.min(count, result);
-
-    return;
-  }
-
-  solution(currentNum + "1", count + 1);
-  solution(currentNum * 2, count + 1);
+  return -1;
 }
 
-solution(startNum, 1);
-
-result = result === Infinity ? -1 : result;
-
-console.log(result);
+console.log(solution(input));
