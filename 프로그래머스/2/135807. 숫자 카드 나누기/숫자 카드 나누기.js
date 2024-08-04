761d030bc3d930b95acd5a1008c1121ca2a16481
OH SeongHo <tjd985@gmail.com>
2024-08-04 23:34:40 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 1256.43 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
@@ -0,0 +1,65 @@
+function solution(arrayA, arrayB) {
+  const result = [];
+  
+  arrayA.sort((a, b) => a - b);
+  arrayB.sort((a, b) => a - b);
+  
+  const smallestAFactors = getFactors(arrayA[0]);
+  const smallestBFactors = getFactors(arrayB[0]);
+  
+  getResult(smallestAFactors, "A");
+  getResult(smallestBFactors, "B");
+  
+  return result.length ? Math.max(...result) : 0;
+  
+  function getResult(targetFactorList, FactorType) {
+    for (const factor of targetFactorList) {
+      // console.log(factor);
+      
+      let isContinue = false;
+
+      for (const value of FactorType === "A" ? arrayA : arrayB) {
+        // console.log(value);
+        if (value % factor) {
+          isContinue = true;
+
+          break;
+        }
+      }
+
+      if (!isContinue) {
+        for (const value of FactorType === "A" ? arrayB : arrayA) {
+          if (!(value % factor)) {
+            isContinue = true;
+
+            break;
+          }
+        }
+      }
+
+      if (isContinue) {
+        isContinue = false;
+
+        continue;
+      }
+
+      result.push(factor);
+
+      break;
+    }
+  }
+}
+
+function getFactors(targetNumber) {
+  const result = [];
+  
+  for (let i = targetNumber; i >= 1; i--) {
+    if (targetNumber % i) {
+      continue;
+    }
+    
+    result.push(i);
+  }
+  
+  return result;
+}
