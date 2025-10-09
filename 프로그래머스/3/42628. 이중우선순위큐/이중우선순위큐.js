07c60e7bc9636e1219be419b0c111846d3443e7c
SEONGHO OH <tjd985@gmail.com>
2025-10-09 16:44:37 +0900

    [level 3] Title: 이중우선순위큐, Time: 50.86 ms, Memory: 51.4 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.js
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.js
@@ -1,30 +1,145 @@
-function solution(operations) {
-    const queue = [];
-    
-    for (const operator of operations) {
-        const [char, num] = operator.split(" ");
-        
-        if (char === 'I') {
-            const targetNumber = Number(num);            
-            const index = queue.findIndex((value) => targetNumber < value);
+function addValue(value) {
+  // 노드가 없을 경우
+  if (this.value === null) {
+    this.value = value;
+    return;
+  }
+  
+  if (value > this.value) {
+    if (!this.right) {
+      const newNode = new Node(value);
+      newNode.parent = this;
+      this.right = newNode;
+      return;
+    }
+    this.right.addValue(value);
+    return;
+  }
+  
+  if (!this.left) {
+    const newNode = new Node(value);
+    newNode.parent = this;
+    this.left = newNode;
+    return;
+  }
+  this.left.addValue(value);
+}
 
-            if (index === -1) {
-              queue.push(targetNumber);
-            } else {
-              queue.splice(index, 0, targetNumber);
-            }
-        }
+function removeMinValue() {
+  // 빈 트리 체크
+  if (this.value === null) return;
+  
+  if (!this.left) {
+    // root 노드를 삭제하는 경우
+    if (!this.parent) {
+      if (this.right) {
+        // 오른쪽 자식이 있으면 그 값을 root로
+        this.value = this.right.value;
+        this.left = this.right.left;
+        const oldRight = this.right.right;
+        this.right = oldRight;
         
-        if (char === 'D') {
-            if (num === '-1') {
-                queue.shift();
-            }
-            
-            if (num === '1') {
-                queue.pop();
-            }
-        }
+        if (this.left) this.left.parent = this;
+        if (this.right) this.right.parent = this;
+      } else {
+        // 자식이 없으면 빈 트리로
+        this.value = null;
+      }
+      return;
     }
     
-    return queue.length ? [queue[queue.length - 1], queue[0]] : [0, 0];
+    this.parent.left = this.right;
+    if (this.right) {
+      this.right.parent = this.parent;
+    }
+    return;
+  }
+  
+  this.left.removeMinValue();
+}
+
+function removeMaxValue() {
+  // 빈 트리 체크
+  if (this.value === null) return;
+  
+  if (!this.right) {
+    // root 노드를 삭제하는 경우
+    if (!this.parent) {
+      if (this.left) {
+        // 왼쪽 자식이 있으면 그 값을 root로
+        this.value = this.left.value;
+        this.right = this.left.right;
+        const oldLeft = this.left.left;
+        this.left = oldLeft;
+        
+        if (this.left) this.left.parent = this;
+        if (this.right) this.right.parent = this;
+      } else {
+        // 자식이 없으면 빈 트리로
+        this.value = null;
+      }
+      return;
+    }
+    
+    this.parent.right = this.left;
+    if (this.left) {
+      this.left.parent = this.parent;
+    }
+    return;
+  }
+  
+  this.right.removeMaxValue();
+}
+
+function getMinValue() {
+  if (this.value === null) return null;
+  if (!this.left) return this.value;
+  return this.left.getMinValue();
+}
+
+function getMaxValue() {
+  if (this.value === null) return null;
+  if (!this.right) return this.value;
+  return this.right.getMaxValue();
+}
+
+class Node {
+  constructor(value = null) {
+    this.value = value;
+    this.parent = null;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+Node.prototype.addValue = addValue;
+Node.prototype.removeMinValue = removeMinValue;
+Node.prototype.removeMaxValue = removeMaxValue;
+Node.prototype.getMinValue = getMinValue;
+Node.prototype.getMaxValue = getMaxValue;
+
+function solution(operations) {
+  const root = new Node();
+  
+  operations.forEach((operator) => {
+    const [char, num] = operator.split(" ");
+    
+    switch (char) {
+      case 'I':
+        root.addValue(Number(num));
+        
+        break;
+        
+      case 'D':
+        if (num === '1') {
+          root.removeMaxValue();
+          
+          break;
+        }
+        
+        root.removeMinValue();
+    }
+  });
+
+  return [root.getMaxValue() ?? 0, root.getMinValue() ?? 0];
 }
