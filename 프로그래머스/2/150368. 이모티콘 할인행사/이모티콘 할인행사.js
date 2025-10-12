b67d8dd1644bad34a7481b3f90c25e73f3f2d2dc
SEONGHO OH <tjd985@gmail.com>
2025-10-12 16:36:55 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 115.36 ms, Memory: 48.4 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
@@ -1,5 +1,5 @@
 function calculatePrice(price, sale) {
-    return price * (100 - sale) / 100;
+  return price * (100 - sale) / 100;
 }
 
 function getPriceResult(result) {
@@ -16,57 +16,46 @@ function getPriceResult(result) {
 }
 
 function solution(users, emoticons) {
-    const priceCacheMap = {};
-    const SALE_PERCENT_LIST = [10, 20, 30, 40];
-    let plusCount = 0;
-    let priceSum = 0;
-    
-    function dfs(users, depth, salePercent) {
-        const emoticon = emoticons[depth];
-        let salePrice = priceCacheMap[`${emoticon}-${salePercent}`];
-        
-        if (!salePrice) {
-            salePrice = calculatePrice(emoticon, salePercent);
-            
-            priceCacheMap[`${emoticon}-${salePercent}`] = salePrice;
-        }
-        
-        const result = users.map((user) => {
-            const currentUser = [...user];
-            
-            if (salePercent >= currentUser[0]) {
-                currentUser[2] = (currentUser[2] ?? 0) + salePrice;
-            }
-            
-            return currentUser;
-        });
+  const priceCacheMap = {};
+  const SALE_PERCENT_LIST = [10, 20, 30, 40];
+  let plusCount = 0;
+  let priceSum = 0;
+
+  function dfs(users, depth) {
+    if (depth === emoticons.length) {
+      const [plus, sum] = getPriceResult(users);
       
-        if (depth === emoticons.length - 1) {
-              const [plus, sum] = getPriceResult(result);
+      if (plus > plusCount || (plus === plusCount && sum > priceSum)) {
+        plusCount = plus;
+        priceSum = sum;
+      }
 
-              if (plus > plusCount) {
-                  plusCount = plus;
-                  priceSum = sum;
-
-                  return;
-              }
-
-              if (plus === plusCount) {
-                  priceSum = Math.max(priceSum, sum);
-              }
-          
-            return;
-          }
-        
-        
-        for (const sale of SALE_PERCENT_LIST) {
-            dfs(result, depth + 1, sale);
-        }
+      return;
     }
-    
-    for (const sale of SALE_PERCENT_LIST) {
-        dfs(users, 0, sale);
+
+    for (const salePercent of SALE_PERCENT_LIST) {
+      const emoticon = emoticons[depth];
+      let salePrice = priceCacheMap[`${emoticon}-${salePercent}`];
+
+      if (!salePrice) {
+        salePrice = calculatePrice(emoticon, salePercent);
+
+        priceCacheMap[`${emoticon}-${salePercent}`] = salePrice;
+      }
+
+      const result = users.map(([ratio, maximum, spend = 0]) => {
+        return [
+          ratio,
+          maximum,
+          salePercent >= ratio ? spend + salePrice : spend,
+        ]
+      });
+      
+      dfs(result, depth + 1);
     }
-    
-    return [plusCount, priceSum];
+  }
+  
+  dfs(users, 0);
+
+  return [plusCount, priceSum];
 }
